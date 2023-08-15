efc38e809d79121de7e2ab636283ac935c5a65de
girish100 <40469301+girish100@users.noreply.github.com>
2023-08-15 16:14:20 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+str1 = 'software engineer'
+str = 'core python'
+#print('slice operator in  forward (+ve) direction')
+print()
+#lookingfor = 'e'
+j = -len(str)
+print(j)
+for i in range(len(str)):
+    print(str[i],'->',j, end= " ")
+    j += 1
+print()
